8e3be37103a635b2dd9b1f1b9ac831e1176ab496
Animesh Jha <animeshjha95166@gmail.com>
2026-09-01 03:16:19 +0530

    create frontend folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar";
+
+function App(){
+  return(
+    <div> 
+      <Navbar />
+
+      <h1>Photo Sharing website</h1>
+      <p>Welcome to the dashboard</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <nav>
+            <h2>PhotoShare</h2>
+
+            <div>
+                <button>Home</button>
+                <button>Upload</button>
+                <button>Profile</button>
+                <button>Login</button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
